0c9e13a0be8d7dac28483f31a3c172f44348a0d6
DeadPackets <deadpackets@protonmail.com>
2018-03-31 22:10:02 +0400

    Removed old username

--- ddoscheck.sh
+++ ddoscheck.sh
@@ -4,7 +4,7 @@
 
 SITE=$1
 echo '----------------------------------------'
-echo '| DDos Checker by shadowblade7536 v1.0 |'
+echo '| DDos Checker by DeadPackets v1.0 |'
 echo '----------------------------------------'
 echo ''
 curl --silent isup.me/$SITE| sed -e 's/<[^>]*>//g' | grep $SITE | sed 's/......//' |  sed 's/[\#;:&-]//g' | sed 's/httpx2Fx2F//g'
